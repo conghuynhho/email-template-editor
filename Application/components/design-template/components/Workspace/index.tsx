4df169e9d9ab4b40a6cf8e2de94f59f682fa20e8
taint@antsprogrammatic.com <taint@antsprogrammatic.com>
2021-02-23 14:59:53 +0700

    dàn layout workspace sidepanel

--- Application/components/design-template/components/Workspace/index.tsx
+++ Application/components/design-template/components/Workspace/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import classnames from 'classnames';
+import styles from './styles.module.scss';
 
 const Workspace = () => {
     return (
         <div>
-            this is Workspace
+            <div className={classnames(styles['inner-content'])}>
+                This is template
+            </div>
         </div>
     );
 };
--- Application/components/design-template/index.tsx
+++ Application/components/design-template/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import classnames from 'classnames';
 import SidePanel from './components/SidePanel';
 import Workspace from './components/Workspace';
+import styles from './styles.module.scss';
 
 const DesignTemplate = () => {
     return (
-        <div>
-            <SidePanel />
-            <Workspace />
+        <div className={classnames(styles['grid-container'])}>
+            <div className={classnames(styles['grid-workspace'])}>
+                <Workspace />
+            </div>
+
+            <div className={classnames(styles['grid-sidepanel'])}>
+                <SidePanel />
+            </div>
         </div>
     );
 };
