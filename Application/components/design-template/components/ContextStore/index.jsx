4f1c269c1831a5c1db1372a76cca395112b4e5aa
taint@antsprogrammatic.com <taint@antsprogrammatic.com>
2021-02-26 17:16:51 +0700

    add function update to store

--- Application/components/design-template/components/ContextStore/index.jsx
+++ Application/components/design-template/components/ContextStore/index.jsx
@@ -28,6 +28,24 @@ const reducer = (state, action) => {
 
             return {...newState};
         }
+        case actionType.UPDATE_BODY: {
+            if (!getObjectPropSafely(() => Object.keys(payload.values).length && payload.id)) {
+                return state;
+            }
+
+            const bodies = produce(state.rows, draft => {
+                if (draft[payload.id]) {
+                    draft[payload.id] = {...draft[payload.id], ...payload.values};
+                }
+            });
+
+            const newState = {
+                ...state,
+                bodies: bodies
+            };
+
+            return {...newState};
+        }
         case actionType.UPDATE_ROW: {
             if (!getObjectPropSafely(() => Object.keys(payload.values).length && payload.id)) {
                 return state;
@@ -42,6 +60,42 @@ const reducer = (state, action) => {
             const newState = {
                 ...state,
                 rows: rows
+            };
+
+            return {...newState};
+        }
+        case actionType.UPDATE_COLUMN: {
+            if (!getObjectPropSafely(() => Object.keys(payload.values).length && payload.id)) {
+                return state;
+            }
+
+            const columns = produce(state.rows, draft => {
+                if (draft[payload.id]) {
+                    draft[payload.id] = {...draft[payload.id], ...payload.values};
+                }
+            });
+
+            const newState = {
+                ...state,
+                columns: columns
+            };
+
+            return {...newState};
+        }
+        case actionType.UPDATE_CONTENT: {
+            if (!getObjectPropSafely(() => Object.keys(payload.values).length && payload.id)) {
+                return state;
+            }
+
+            const contents = produce(state.rows, draft => {
+                if (draft[payload.id]) {
+                    draft[payload.id] = {...draft[payload.id], ...payload.values};
+                }
+            });
+
+            const newState = {
+                ...state,
+                contents: contents
             };
 
             return {...newState};
